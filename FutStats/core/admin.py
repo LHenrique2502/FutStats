b37145489c6c1f98aac4e8f6e88f48931d94a5a2
Lucas Henrique <henrique.sp.lucas@gmail.com>
2026-01-12 23:05:36 -0300

    Remove duplicated FutStats folder (uppercase)

--- FutStats/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import League, Team, Player, Match
-
-admin.site.register(League)
-admin.site.register(Team)
-admin.site.register(Player)
-admin.site.register(Match)
-
--- FutStats/test_env.py
+++
@@ -1,4 +0,0 @@
-import psycopg2
-import os
-
-conn = psycopg2.connect(os.environ["DATABASE_URL"])
